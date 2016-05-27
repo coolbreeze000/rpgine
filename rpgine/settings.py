2139803ddaec0d8fc1cbab80e6495f567c6e8a8f
apatecherkel <dominik.herkel@apa.at>
2016-05-27 14:24:17 +0200

    enabled local django auth with sqlite user backend

--- rpgine/settings.py
+++ rpgine/settings.py
@@ -107,14 +107,13 @@ DATABASES = {
 }
 """
 
-"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-"""
+
 
 # TODO uncomment to enable mongodb session backend --> should later be moved to redis
 #SESSION_ENGINE = 'mongoengine.django.sessions' # optional
