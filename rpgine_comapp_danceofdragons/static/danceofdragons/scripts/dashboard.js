706951c7bc34cfb24ea16068e91c9b84b4cd6343
Dominik <dominikherkel@yahoo.de>
2016-06-01 00:27:03 +0200

    implemented own sidebar

--- rpgine_comapp_danceofdragons/static/danceofdragons/scripts/dashboard.js
+++ rpgine_comapp_danceofdragons/static/danceofdragons/scripts/dashboard.js
@@ -4,11 +4,19 @@
 
 console.log("JQUERY")
 
-$( document ).ready(function() {
+$(document).ready(function() {
     $("div#split-bar").on("click",function(e){
-        e.preventDefault()
-        console.log("CLICKED!")
-        $("div#sidebar-wrapper").hide();
-        $("div#split-bar").show();
+        console.log("COLLAPSE");
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+        //$("div#split-bar").css({"left":"250px","position":"absolute","width":"1000px"});
+        $('<div id="split-bar-return"></div>').insertBefore("div#wrapper");
+        //$("#wrapper").removeClass("toggled");
+    });
+
+    $("div#split-bar-return").on("click",function(e){
+        console.log("RETURN");
+        e.preventDefault();
+        $("#wrapper").removeClass("toggled");
     });
 });
