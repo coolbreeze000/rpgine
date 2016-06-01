e2fb6de5ccb3c356a584b0095e28fa3618532d25
apatecherkel <dominik.herkel@apa.at>
2016-06-01 16:27:47 +0200

    implemented split-bar-sidebar hide and show

--- rpgine_comapp_danceofdragons/static/danceofdragons/scripts/dashboard.js
+++ rpgine_comapp_danceofdragons/static/danceofdragons/scripts/dashboard.js
@@ -2,21 +2,21 @@
  * Created by Dominik on 25.05.2016.
  */
 
-console.log("JQUERY")
-
 $(document).ready(function() {
-    $("div#split-bar").on("click",function(e){
-        console.log("COLLAPSE");
+    $("div.split-bar-sidebar").on("click",function(e){
         e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
-        //$("div#split-bar").css({"left":"250px","position":"absolute","width":"1000px"});
-        $('<div id="split-bar-return"></div>').insertBefore("div#wrapper");
-        //$("#wrapper").removeClass("toggled");
-    });
 
-    $("div#split-bar-return").on("click",function(e){
-        console.log("RETURN");
-        e.preventDefault();
-        $("#wrapper").removeClass("toggled");
+        if ($('#sidebar').css('display') == 'none')
+        {
+            $("#split-bar-sidebar").css({"margin-left":"160px"});
+            $("#sidebar").css({"display":"initial"});
+            $("div#page-content-wrapper.page-content-wrapper").css({"padding-left":"40px"});
+        }
+        else
+        {
+            $("#split-bar-sidebar").css({"margin-left":"-10px"});
+            $("#sidebar").css({"display":"none"});
+            $("div#page-content-wrapper.page-content-wrapper").css({"padding-left":"-20px"});
+        }
     });
 });
