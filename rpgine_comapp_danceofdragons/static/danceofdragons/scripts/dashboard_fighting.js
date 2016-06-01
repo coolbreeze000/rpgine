df350cdb9db65f56c86b342ddb5d7e54e27e2e57
arieppel <arieppel@gmail.com>
2016-06-02 00:15:44 +0200

    First work in progress version of horizontal splitbar

--- rpgine_comapp_danceofdragons/static/danceofdragons/scripts/dashboard_fighting.js
+++ rpgine_comapp_danceofdragons/static/danceofdragons/scripts/dashboard_fighting.js
@@ -1,3 +1,19 @@
 /**
  * Created by Dominik on 29.05.2016.
  */
+$(document).ready(function() {
+     $("div.split-bar-horizontal-top").on("click",function(e){
+        e.preventDefault();
+
+        if ($('#headline').css('display') == 'none')
+        {
+            $("#split-bar-horizontal-top").css({"margin-top":"100px"});
+            $("#headline").css({"display":"initial"});
+        }
+        else
+        {
+            $("#split-bar-horizontal-top").css({"margin-top":"-100px"});
+            $("#headline").css({"display":"none"});
+        }
+    });
+});
