c2e59dccc89e74dbed6e6a27e9883aad37e89efc
Dominik <dominikherkel@yahoo.de>
2016-06-05 21:03:33 +0200

    added dices

--- rpgine_comapp_danceofdragons/model/diceroller/handlers/DiceHandlerDanceOfDragons.py
+++ rpgine_comapp_danceofdragons/model/diceroller/handlers/DiceHandlerDanceOfDragons.py
@@ -3,7 +3,12 @@ from rpgine_comapp_danceofdragons.model.diceroller.handlers.DiceHandlerGeneric i
 from rpgine_comapp_danceofdragons.model.diceroller.dices.DiceTen import DiceTen
 
 class DiceHandlerDanceOfDragons(DiceHandlerGeneric):
-    DICE_TYPES = {DiceTen.__class__}
+    DEFAULT_VALUES = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    DEFAULT_SUCCESSES = [0, 8, 9]
+    DEFAULT_REROLLS = [0]
 
     def __init__(self):
         DiceHandlerGeneric.__init__()
+
+    def roll(self, values = DEFAULT_VALUES, successes = DEFAULT_SUCCESSES, rerolls = DEFAULT_REROLLS):
+        pass
--- rpgine_comapp_danceofdragons/model/diceroller/handlers/DiceHandlerGeneric.py
+++ rpgine_comapp_danceofdragons/model/diceroller/handlers/DiceHandlerGeneric.py
@@ -1,3 +1,6 @@
 class DiceHandlerGeneric:
     def __init__(self):
         pass
+
+    def roll(self, values):
+        pass
