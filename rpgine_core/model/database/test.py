0230bec8b70fcb577d1c7253dde888bc085ca8b8
arieppel <arieppel@gmail.com>
2016-06-13 20:17:29 +0200

    Begin of sample character in mongoDB for later conversion to json and then xml. This will be our baseline for future xml templates that are imported.

--- rpgine_core/model/database/test.py
+++ rpgine_core/model/database/test.py
@@ -1,8 +1,19 @@
 from PersistenceAPI import DBHandler
 
 driver = DBHandler("192.168.99.100","27017").getConnection()
-db = driver["test_db"]
-db["test_collection"].insert_one({"asd":"asd"})
-result = db["test_collection"].find()
-for x in result:
-    print(x)
+db = driver["Dance_of_Dragons_RPG_DB"]
+db["characters"].insert_one({
+    "player_characters": [{
+        "nelia": [{
+            "properties": [{
+                "display_name": "Nelia Himmelsstein"}, {
+                "attributes": [{
+                    "strength": [{
+                        "display_name": "Strength"}, {
+                        "value": "3"}
+                    ]}
+                ]}
+            ]}
+        ]}
+    ]}
+)
